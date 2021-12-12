f74c654e1b7623ae1616cdf34bf94e7c319272d9
helenwijaya95 <helen.wijaya95@gmail.com>
2021-12-12 16:30:55 +0700

    Install firebase, init firebase functionalities, create upload form

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+  // Import the functions you need from the SDKs you need
+  import * as firebase from 'firebase/app';
+  import 'firebase/storage';
+  import 'fireabse/firestore'
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCacrWIvJepN_0ORw8yIQoBc5bRw3ZMhUE",
+    authDomain: "ninja-firegram-40954.firebaseapp.com",
+    projectId: "ninja-firegram-40954",
+    storageBucket: "ninja-firegram-40954.appspot.com",
+    messagingSenderId: "42962727383",
+    appId: "1:42962727383:web:80c749dbca9c0f40ecdb92"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFireStore = firebase.firestore();
+
+export { projectStorage, projectFireStore };
